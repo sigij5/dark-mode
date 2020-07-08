d102e337fceb8e173f342abee426369011fd19ad
Sigi Jaeckel <sigij55@gmail.com>
2020-07-08 18:01:37 -0400

    added coin price and changed color

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -8,6 +8,7 @@ const Charts = ({ coinData }) => {
         <div className="chart__container" key={coin.name}>
           <h2 className="coin__title">{coin.name}</h2>
           <h4 className="coin__symbol">{coin.symbol}</h4>
+          <h3 className='coin_price'>Current Price:&nbsp;${coin.current_price}</h3>
           <div className="coin__logo">
             <img src={coin.image} height="40" alt={coin.name} />
           </div>
